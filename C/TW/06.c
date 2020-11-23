04ac70a698a83d5a918c32a25c35f769183c8ba4
Fuxx-1 <583742849@qq.com>
2020-11-23 18:21:52 +0800

    2020-11-23

--- C/TW/06.c
+++ C/TW/06.c
@@ -5,58 +5,68 @@ int main(void)
     int n;
     scanf("%d", &n);
     int m = n + n - 1;
-    int q = n;
-    int t = 0;
+    int q = n - 1;
+    int t = 1;
     int a[n][n];
     int *p;
-    p = &a[0][-1];
+    p = &a[0][0];
+    int time = 1;
     for (int i = 0; i < (n + n - 1); i++)
     {
-        if (m % 4 == 2)
+        if (time % 4 == 1)
         {
-            for (int j = 1; j < q; j++)
+            for (int j = 0; j < q; j++)
             {
-                p -= n;
-                t += 1;
                 *p = t;
-            }
-        }
-        else if (m % 4 == 1)
-        {
-            for (int j = 1; j < q; j++)
-            {
                 p += 1;
                 t += 1;
                 *p = t;
+                printf("%p\n", p);
             }
         }
-        else if (m % 4 == 0)
+        else if (time % 4 == 2)
         {
-            for (int j = 1; j < q; j++)
+            for (int j = 0; j < q; j++)
             {
+                *p = t;
                 p += n;
                 t += 1;
                 *p = t;
+                printf("%p\n", p);
             }
         }
-        else if (m % 4 == 3)
+        else if (time % 4 == 3)
         {
-            for (int j = 1; j < q; j++)
+            for (int j = 0; j < q; j++)
             {
+                *p = t;
                 p -= 1;
                 t += 1;
                 *p = t;
+                printf("%p\n", p);
             }
         }
-        if (m % 2)
+        else if (time % 4 == 0)
+        {
+            for (int j = 0; j < q; j++)
+            {
+                *p = t;
+                p -= n;
+                t += 1;
+                *p = t;
+                printf("%p\n", p);
+            }
+        }
+
+        if ((m % 2 == 1)&& time != 1)
             q -= 1;
-        m -= 1;
+        time += 1;
     }
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf("%-5d", a[i][j]);
+            printf("%-10d", a[i][j]);
         }
         printf("\n");
     }
