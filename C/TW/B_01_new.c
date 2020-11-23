5b6abfef1ec818c159bbbf36734350716dc698d2
Fuxx-1 <583742849@qq.com>
2020-11-23 22:13:39 +0800

    2020-11-23

---
+++ C/TW/B_01_new.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    double t, n, s, r, k;
+    scanf("%d", &t);
+    scanf("%d", &n);
+    scanf("%d", &s);
+    scanf("%d", &r);
+    scanf("%d", &k);
+    for ( ; 1 ; )
+    {
+        
+    }
+    
+    
+    
+    system("pause");
+    return 0;
+}
