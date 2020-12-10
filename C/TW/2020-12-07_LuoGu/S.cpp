f6abdf0412cf8a49ac6be1d48355b74c2fe52394
Fuxx-1 <583742849@qq.com>
2020-12-10 21:08:02 +0800

    2020-12-10

---
+++ C/TW/2020-12-07_LuoGu/S.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <algorithm>
+int a[1000000], b[1000000];
+int main(void)
+{
+    int n, m, count, min, val = 0;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+    std::sort(a, a + n);
+    std::sort(b, b + m);
+    int i = 0, j = 0;
+    while (i < n)
+    {
+        while (j < m)
+        {
+            if (a[i] >= b[j] && a[i] <= b[j + 1])
+            {
+                val += a[i] - b[j] > b[j + 1] - a[i] ? b[j + 1] - a[i] : a[i] - b[j];
+                break;
+            }
+            else if (a[i] < b[j])
+            {
+                val += b[j] - a[i];
+                break;
+            }
+            else if (j - m)
+            {
+                j++;
+                break;
+            }
+            else
+            {
+                val += a[i] - b[j];
+            }
+        }
+        i++;
+    }
+    printf("%d", val);
+    return 0;
+}
