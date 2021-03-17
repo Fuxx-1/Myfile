64a3c89c8f93e03ad35013a5981c6701c90c6e58
Fuxx-1 <583742849@qq.com>
2021-03-17 21:44:39 +0800

    2021-03-17

--- C/Solve/TB.c
+++ C/Solve/TB.c
@@ -11,7 +11,7 @@ int main(void)
     system("call git add --all");
     system("call git commit -m \"%date:~3,4%-%date:~8,2%-%date:~11,2%\"");
     printf("*****更改已提交，等待推送*****\n");
-    int a = 300;
+    int a = 100;
     while (a < 200)
     {
         a = printf(system("call git push origin main"));
