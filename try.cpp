6585956c9bc87c0a0419dcdf6d1bb6f02c930629
Fuxx-1 <583742849@qq.com>
2021-03-18 21:12:40 +0800

    2021-03-18

--- try.cpp
+++ try.cpp
@@ -1,8 +1,42 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <stdlib.h>
+#define N 500
+
+void Xc(int *a, int n)
+{
+    int flag, k, plus;
+    flag = N - 1;
+    a[N] = 1;
+    for (int i = 1; i < n + 1; i++)
+    {
+        plus = 0;
+        for (int j = N - 1; j < flag; j--)
+        {
+            k = a[j] * i + plus;
+            a[j] = k % 10;
+            plus = k / 10;
+        }
+        while (plus)
+        {
+            a[flag] = plus % 10;
+            flag--;
+            plus = plus / 10;
+        }
+    }
+    for (int i = N - 1; i > flag; i--)
+    {
+        printf("%d", a[i]);
+    }
+}
+
 int main(void)
 {
-    printf("%d", printf("China"));
+
+    int a[N], n;
+    printf("请输入n的阶乘：\n");
+    scanf("%d", &n);
+
+    Xc(a, n);
+
     return 0;
 }
