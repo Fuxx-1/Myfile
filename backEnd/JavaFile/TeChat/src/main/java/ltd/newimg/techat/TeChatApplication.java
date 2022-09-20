4a2b735e1457672a8b77366881df5f80b69d1533
Fuxx-1 <583742849@qq.com>
2022-09-20 23:19:03 +0800

    2022-09-20

---
+++ backEnd/JavaFile/TeChat/src/main/java/ltd/newimg/techat/TeChatApplication.java
@@ -0,0 +1,13 @@
+package ltd.newimg.techat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeChatApplication.class, args);
+    }
+
+}
---
+++ backEnd/JavaFile/TeChat/src/test/java/ltd/newimg/techat/TeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ltd.newimg.techat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
