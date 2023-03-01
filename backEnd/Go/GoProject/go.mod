cdd3bb2ec5ece8a34a86e04df0433fa49440994b
Fuxx <583742849@qq.com>
2023-03-01 23:54:23 +0800

    2023-03-01

--- backEnd/Go/GoProject/go.mod
+++ backEnd/Go/GoProject/go.mod
@@ -3,11 +3,13 @@ module GoProject
 go 1.19
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/tidwall/gjson v1.14.4
-	golang.org/x/text v0.6.0
+	golang.org/x/text v0.7.0
 )
 
 require (
 	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 )
--- backEnd/JavaFile/recruitSystem/src/test/java/cn/lab/recruitsystem/ServiceTest/UserServiceTest.java
+++ backEnd/JavaFile/recruitSystem/src/test/java/cn/lab/recruitsystem/ServiceTest/UserServiceTest.java
@@ -1,31 +1,30 @@
-package cn.lab.recruitsystem.ServiceTest;
-
-import cn.lab.recruitsystem.service.UserService;
-import com.alibaba.fastjson.JSONObject;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import javax.annotation.Resource;
-
-/**
- * @author Fuxx-1@Github
- * @Description
- * @create 2022-05-05 20:04
- */
-@SpringBootTest
-public class UserServiceTest {
-    @Resource
-    UserService userService;
-
-    @Test
-    public void sendVerifyEmail() {
-        ApplicationContext applicationContext =
-        System.out.println(userService.sendVerifyEmail("583742849@qq.com", "Test"));
-    }
-
-    @Test
-    public void verifyCode() {
-        System.out.println(userService.verifyCode("583742849@qq.com", "343292"));
-    }
-}
+//package cn.lab.recruitsystem.ServiceTest;
+//
+//import cn.lab.recruitsystem.service.UserService;
+//import com.alibaba.fastjson.JSONObject;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.ApplicationContext;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * @author Fuxx-1@Github
+// * @Description
+// * @create 2022-05-05 20:04
+// */
+//@SpringBootTest
+//public class UserServiceTest {
+//    @Resource
+//    UserService userService;
+//
+//    @Test
+//    public void sendVerifyEmail() {
+//        ApplicationContext applicationContext = System.out.println(userService.sendVerifyEmail("583742849@qq.com", "Test"));
+//    }
+//
+//    @Test
+//    public void verifyCode() {
+//        System.out.println(userService.verifyCode("583742849@qq.com", "343292"));
+//    }
+//}
