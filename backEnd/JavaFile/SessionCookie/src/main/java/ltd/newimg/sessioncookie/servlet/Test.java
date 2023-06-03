5c26e52e276aa6bceb8bdfa1f75202b70a4321ac
Fuxx-1 <583742849@qq.com>
2023-06-03 15:35:45 +0800

    2023-06-03

--- backEnd/JavaFile/SessionCookie/src/main/java/ltd/newimg/sessioncookie/servlet/Test.java
+++ backEnd/JavaFile/SessionCookie/src/main/java/ltd/newimg/sessioncookie/servlet/Test.java
@@ -2,38 +2,47 @@ package ltd.newimg.sessioncookie.servlet;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Test {
     private static TestLock testLock = new TestLock();
+    private static ReadWriteLock lock = new ReentrantReadWriteLock();
     private static int num = 0;
     public static void main(String[] args) throws InterruptedException {
-//        for (int i = 0; i < 5; i++) {
-//            new Thread(()-> {
-//                testLock.readlock();
-//                System.out.println(Thread.currentThread().getName() + "start!");
-//                System.out.println(num);
-//                System.out.println(Thread.currentThread().getName() + "over!");
-//                testLock.readunlock();
-//            }).start();
-//        }
-        for (int i = 0; i < 5; i++) {
-            new Thread(()-> {
-                testLock.writelock();
-                System.out.println(Thread.currentThread().getName() + "start!");
-                System.out.println(num + "->" + ++num);
-                System.out.println(Thread.currentThread().getName() + "over!");
-                testLock.writeunlock();
-            }).start();
+        lock.readLock().lock();
+        for (int i = 0; i < 20; i++) {
+            add();
         }
-        TimeUnit.SECONDS.sleep(5);
         for (int i = 0; i < 5; i++) {
-            new Thread(()-> {
+            read();
+        }
+    }
+
+    private static void read() {
+        new Thread(()-> {
+            try {
                 testLock.readlock();
-                System.out.println(Thread.currentThread().getName() + "start!");
-                System.out.println(num);
-                System.out.println(Thread.currentThread().getName() + "over!");
-                testLock.readunlock();
-            }).start();
-        }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + "start!");
+            System.out.println(num);
+            System.out.println(Thread.currentThread().getName() + "over!");
+            testLock.readunlock();
+        }).start();
+    }
+
+    private static void add() {
+        new Thread(()-> {
+            try {
+                testLock.writelock();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + "start!");
+            System.out.println(num + "->" + ++num);
+            System.out.println(Thread.currentThread().getName() + "over!");
+            testLock.writeunlock();
+        }).start();
     }
 }
