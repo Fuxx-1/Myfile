7a3e320e80aadab94e3ebfa957a8a05d706eee3d
Fuxx-1 <583742849@qq.com>
2023-06-13 12:18:36 +0800

    2023-06-13

--- backEnd/JavaFile/helloworld/src/main/java/ltd/newimg/controller/TestController.java
+++ backEnd/JavaFile/helloworld/src/main/java/ltd/newimg/controller/TestController.java
@@ -1,18 +1,27 @@
 package ltd.newimg.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestController {
 
+    @ResponseBody
     @RequestMapping("/test1")
     public String test1() {
         return "test1";
     }
 
+    @ResponseBody
     @RequestMapping("/test2")
     public String test2() {
         return "test2";
+    }
+
+    @RequestMapping("/test3")
+    public String test(){
+        return "index.html";
     }
 }
