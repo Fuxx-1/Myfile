a6cae8d09950b533b4d55802a1a02f7043f341c7
Fuxx-1 <583742849@qq.com>
2023-09-19 20:45:29 +0800

    2023-09-19

--- backEnd/JavaFile/untitled/src/main/java/oj/Solution.java
+++ backEnd/JavaFile/untitled/src/main/java/oj/Solution.java
@@ -3,56 +3,37 @@ package oj;
 import java.util.*;
 
 public class Solution {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] s = sc.nextLine().replaceAll("[\"]", "").split(",");
-        String res = minWindow(s[0], s[1]);
-        if (res.length() == 0) System.out.println("\"\"");
-        else System.out.println("\"" + res + "\"");
+        int[] nums = new int[]{1, 2, 3};
+        System.out.println(permute(nums));
     }
 
-    public static String minWindow(String source, String target) {
-        // 初始化counter_s和counter_t
-        Map<Character, Integer> counter_t = new HashMap<Character, Integer>();
-        Map<Character, Integer> counter_s = new HashMap<Character, Integer>();
-        for (int i = 0; i < target.length(); i++) {
-            int count = counter_t.getOrDefault(target.charAt(i), 0);
-            counter_t.put(target.charAt(i), count + 1);
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        List<Integer> output = new ArrayList<Integer>();
+        for (int num : nums) {
+            output.add(num);
         }
 
-        int left = 0, valid = 0;
-        // 记录最小覆盖子串的起始索引及长度
-        int start = -1, minlen = Integer.MAX_VALUE;
-        for (int right = 0; right < source.length(); right++) {
-            // 移动右边界, ch 是将移入窗口的字符
-            char ch = source.charAt(right);
-            if (counter_t.containsKey(ch)) {
-                counter_s.put(ch, counter_s.getOrDefault(ch, 0) + 1);
-                if (counter_s.get(ch).compareTo(counter_t.get(ch)) == 0) {
-                    valid++;
-                }
-            }
-            // 判断左侧窗口是否要收缩
-            while (valid == counter_t.size()) {
-                // 更新最小覆盖子串
-                if (right - left < minlen) {
-                    start = left;
-                    minlen = right - left;
-                }
-                // left_ch 是将移出窗口的字符
-                char left_ch = source.charAt(left);
-                // 左移窗口
-                left++;
-                // 进行窗口内数据的一系列更新
-                if (counter_t.containsKey(left_ch)) {
-                    if (counter_t.get(left_ch).equals(counter_s.get(left_ch))) {
-                        valid--;
-                    }
-                    counter_s.put(left_ch, counter_s.getOrDefault(left_ch, 0) - 1);
-                }
-            }
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    public static void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
         }
-        // 返回最小覆盖子串
-        return start == -1 ? "" : source.substring(start, start + minlen + 1);
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
     }
 }
